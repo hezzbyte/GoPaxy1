f0009a1bbe6cfb1ba2f3e14a85a23a821b3cc905
hezzy <hezzbyte@gmail.com>
2019-09-21 20:42:00 +0100

    ss

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -26,9 +26,9 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-console": "1.0.7",
     "cordova-plugin-statusbar": "1.0.1",
-    "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-splashscreen": "5.0.3"
 }
 // BOTTOM OF METADATA
